2eea0f186cd2cc0ba5830ad03eb3fd5a2fe60878
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-08-26 12:15:58 +0530

    switch statements

---
+++ Practice/switch_statement.java
@@ -0,0 +1,15 @@
+public class switch_statement {
+    public static void main(String[]args){
+        int number=2;
+        switch(number){
+            case 1 : System.out.println("mango");
+                        break;
+            case 3 : System.out.println("banana");
+                        break;
+            case 2 : System.out.println("apple");
+                        break;
+            default : System.out.println("we realize we are dreaming");
+
+        }
+    }
+}
