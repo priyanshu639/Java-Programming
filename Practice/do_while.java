ef08c5a18d66d074243b697019b9b1a57d160ac8
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-08-29 16:53:38 +0530

    do while loops

---
+++ Practice/do_while.java
@@ -0,0 +1,11 @@
+public class do_while {
+    public static void main(String[]args){
+        int num=1;
+        do{
+            System.out.println("hello gys");
+            num++;
+
+        }while(num<=10);
+    }
+    
+}
