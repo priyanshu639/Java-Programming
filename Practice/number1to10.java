82ef6b5a68a2dc1c3507ba70db2740d58978d765
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-08-29 15:57:21 +0530

    printing number from 1 to 10

---
+++ Practice/number1to10.java
@@ -0,0 +1,10 @@
+public class number1to10 {
+    public static void main (String[]args){
+        int cout=1;
+        while(cout<=10){
+            System.out.println(cout);
+            cout++;
+        }
+    }
+    
+}
