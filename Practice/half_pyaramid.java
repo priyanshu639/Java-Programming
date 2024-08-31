feeee0a2e6ae8c13b6312bbe56dc076ce2987df1
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-08-31 19:56:47 +0530

    half pyramid pattern

---
+++ Practice/half_pyaramid.java
@@ -0,0 +1,12 @@
+public class half_pyaramid {
+    public static void main(String[]args){
+        int n=4;
+        for(int line=1;line<=n;line++){
+            for(int count=1;count<=line;count++){
+                System.out.print(count);
+            } 
+            System.out.println();
+        }
+    }
+    
+}
