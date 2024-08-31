cc580586001b925afa96a1a3a965f29fef2aeeed
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-08-31 20:28:59 +0530

    function

---
+++ Practice/function/function1.java
@@ -0,0 +1,15 @@
+
+
+public class function1 {
+
+    public static void Sayhelloworld(){
+        System.out.println("hello world");
+        return;
+    }
+
+    
+    public static void main(String[]args){
+        Sayhelloworld();
+    }
+    
+}
