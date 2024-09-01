42353671a1a53624ece19ee830f7ffbb752cdba9
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-09-01 20:41:04 +0530

    function call by value doing multiply

---
+++ Practice/function/multiplyNUmbers.java
@@ -0,0 +1,12 @@
+public class multiplyNUmbers {
+
+    public static int multiply(int a,int b){
+        int prod =a*b;
+        return prod;
+    }
+    public static void main (String[]args){
+        int product=multiply(5,6);
+        System.out.println(product);
+    }
+    
+}
