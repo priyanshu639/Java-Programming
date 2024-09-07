ef2372f222e0f00ca7ad885774a184a061cfd7f4
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-09-07 13:45:32 +0530

    finding largest number from the array

---
+++ Practice/function/Array/largest_number.java
@@ -0,0 +1,20 @@
+// import java.util.*;
+public class largest_number {
+    public static int getlargest(int arr[]) {
+        int largest = Integer.MIN_VALUE; //minus infinity
+        for(int i=0;i<arr.length;i++){
+            if(largest<arr[i]){
+                largest=arr[i];
+            }
+        }
+            return largest;
+        
+
+        
+    }
+    public static void main(String[] args) {
+        int arr[] ={3,5,8,9,45,6,4,8,32,16};
+        System.out.println("the largest value is " + getlargest(arr));
+        
+    }
+}
