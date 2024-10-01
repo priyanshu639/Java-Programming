7bd4cf389118fd754e3508956879ec6859458212
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-10-02 00:45:37 +0530

    spiral matrix printing in 2 d array

--- Practice/twoDARRAY.java
+++ Practice/twoDARRAY.java
@@ -1,5 +1,16 @@
 import java.util.*;
 public class twoDARRAY {
+    public static boolean search(int arr[][],int key) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[0].length;j++){
+                if(arr[i][j]==key){
+                    System.out.print("cell  found : (" +i +" , "+j+")");
+            return true;
+            }
+        }
+    }   System.out.println("key not found");
+        return  false;       
+    }
     
 
     public static void main (String args[]){
@@ -19,7 +30,10 @@ public class twoDARRAY {
         
         System.out.println();
         }
+        search(arr,5);
     }
+
+
     
 }
 
