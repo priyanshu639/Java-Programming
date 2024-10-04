0f52cb5d4540672fa7d7113cdb248c9ed0537ca5
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-10-04 23:53:39 +0530

    bit manipulation AND OR XOR NOT

---
+++ Practice/Bit_manipulation.java
@@ -0,0 +1,14 @@
+public class Bit_manipulation {
+    public static void main(String[] args) {
+        System.out.println(5&6);
+        System.out.println(5|6);
+        System.out.println(5^6);
+        System.out.println(~5);
+        System.out.println(~0);
+        System.out.println(5<<2);
+        System.out.println(5>>2);
+        System.out.println(6>>2);
+        System.out.println(6>>1);
+    }
+    
+}
