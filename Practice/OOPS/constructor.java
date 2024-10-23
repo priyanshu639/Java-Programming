33bcb4cba4f2a591aec50e18006b4910076d18c0
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-10-23 23:33:07 +0530

    constructor

---
+++ Practice/OOPS/constructor.java
@@ -0,0 +1,15 @@
+public class constructor {
+    public static void main(String[] args) {
+        Std obj = new Std();
+        
+    }
+}
+
+    class Std{
+        String name;
+        int age;
+        Std(){
+            System.out.println("constructor is called");
+        }
+    }
+
