9170207ecbfbc47eb4d36072cd983259d6d8cedb
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2024-12-03 17:08:24 +0530

    arraylist sorting

---
+++ Practice/OOPS/Recursion/ARRAYLIST/sorting_AL.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Collections;
+public class sorting_AL {
+    public static void main(String[] args) {
+        ArrayList<Integer> list=new ArrayList<>();
+            list.add(2);
+            list.add(5);
+            list.add(9);
+            list.add(3);
+            list.add(8);
+            // asscending order
+            Collections.sort(list);
+            System.out.println(list);
+        
+
+            // desccending order
+
+            Collections.sort(list,Collections.reverseOrder());
+            System.out.println(list);
+    }
+    
+}
