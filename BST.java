555958fb85a62de17bd13914f86f53c60cfea6fe
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2025-02-01 23:52:11 +0530

    binary search tree

---
+++ BST.java
@@ -0,0 +1,51 @@
+public class BST {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            // Left subtree
+            root.left = insert(root.left, val);
+        } else {
+            // Right subtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void main(String[] args) {
+        int[] values = {5, 1, 3, 4, 2, 7};
+        Node root = null;
+
+        // Insert all values into the BST
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        // Print the inorder traversal
+        inorder(root);
+        System.out.println();
+    }
+}
