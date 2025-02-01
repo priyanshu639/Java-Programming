f8ad4e92f4ac5cd9c89705b9faacff0314781916
priyanshu639 <154823006+priyanshu639@users.noreply.github.com>
2025-02-01 15:10:16 +0530

    implementation of queue using array

---
+++ LargestHistogramArea.java
@@ -0,0 +1,56 @@
+
+import java.util.Stack;
+
+public class LargestHistogramArea {
+    public static void maxArea(int arr[]) {
+        int maxArea = 0;
+        int nsr[] = new int[arr.length]; // Next Smaller Right
+        int nsl[] = new int[arr.length]; // Next Smaller Left
+
+        // Next Smaller Right
+        Stack<Integer> s = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsr[i] = arr.length;
+            } else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // Next Smaller Left
+        s = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // Compute the maximum area
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(currArea, maxArea);
+        }
+
+        System.out.println("Max area in histogram = " + maxArea);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {2, 1, 5, 6, 2, 3}; // Heights in histogram
+        maxArea(arr);
+    }
+}
+
+    
+
